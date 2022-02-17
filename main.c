1acb8b2ccaae28eaedc59d5ebd32dbcd9b3bb059
Leonardo <leonardo.aa88@gmail.com>
2022-02-16 22:43:51 -0300

    IP headers

--- main.c
+++ main.c
@@ -33,7 +33,7 @@ int main(int argc, char *argv[])
     int raw_socket;
     char buffer[BUFFER_SIZE];
     ssize_t bytes_read;
-    struct sockaddr addr;
+    struct sockaddr addr, source, destination;
     socklen_t addr_size = (socklen_t)sizeof(addr);
     size_t buffer_len;
 
@@ -50,7 +50,7 @@ int main(int argc, char *argv[])
 
     while (true)
     {
-        // memset(buffer, 0, sizeof(buffer));
+        memset(buffer, 0, sizeof(buffer));
         bytes_read = recvfrom(raw_socket, buffer, sizeof(buffer), 0, &addr, &addr_size);
         if (bytes_read < 0)
         {
@@ -66,4 +66,52 @@ int main(int argc, char *argv[])
     close(raw_socket);
 
     return 0;
+}
+
+void process_packet(char *buffer, size_t size)
+{
+  struct iphdr *ip_header = (struct iphdr *) buffer;
+
+  switch (ip_header->protocol) // checks protocol
+  {
+    case 1: // ICMP protocol
+      break;
+
+    case 2: // IGMP protocol
+      break;
+
+    case 6: // TCP protocol
+      break;
+
+    case 17: // UDP protocol
+      break;
+
+    default: // other protocols
+      break;
+  }
+}
+
+void print_ip_header(char *buffer, struct sockaddr source, struct sockaddr destination, size_t size)
+{
+  unsigned short ip_header_length;
+  struct iphdr *ip_header = (struct iphdr *) buffer;
+  ip_header_length = ip_header->ihl*4;
+
+  memset(&source, 0, sizeof(source));
+  source.sin_addr.s_addr = iph->saddr;
+
+  memset(&destination, 0, sizeof(destination));
+  destination.sin_addr.s_addr = iph->daddr;
+
+  printf("IP Header\n");
+	printf("   |-IP Version        : %d\n",(unsigned int)iph->version);
+	printf("   |-IP Header Length  : %d DWORDS or %d Bytes\n",(unsigned int)iph->ihl,((unsigned int)(iph->ihl))*4);
+	printf("   |-Type Of Service   : %d\n",(unsigned int)iph->tos);
+	printf("   |-IP Total Length   : %d  Bytes(Size of Packet)\n",ntohs(iph->tot_len));
+	printf("   |-Identification    : %d\n",ntohs(iph->id));
+	printf("   |-TTL      : %d\n",(unsigned int)iph->ttl);
+	printf("   |-Protocol : %d\n",(unsigned int)iph->protocol);
+	printf("   |-Checksum : %d\n",ntohs(iph->check));
+	printf("   |-Source IP        : %s\n",inet_ntoa(source.sin_addr));
+	printf("   |-Destination IP   : %s\n",inet_ntoa(destination.sin_addr));
 }
